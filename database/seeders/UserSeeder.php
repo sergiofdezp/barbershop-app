93e40e916a96efb11e7c3fca5ac0ae89e2f8a49a
serfgiofdezp <sergiofdezp7@gmail.com>
2024-03-25 20:58:41 +0100

    creacion del userseeder (usuario admin y peluquero)

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // usuario administrador
+        DB::table('users')->insert([
+            'name' => "Admin",
+            'email' => "admin@admin.com",
+            'password' => Hash::make('admin')
+        ]);
+
+        // usuario para peluquero
+        DB::table('users')->insert([
+            'name' => "Peluquero",
+            'email' => "peluquero@peluquero.com",
+            'password' => Hash::make('peluquero')
+        ]);
+    }
+}

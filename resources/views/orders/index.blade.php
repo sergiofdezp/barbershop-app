5b96c94b94150984b332f70d9d61564f7f492784
sergiof <sergiofdezp7@gmail.com>
2024-04-15 19:17:31 +0200

    cambios en el index de las orders

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -1,5 +1,15 @@
 <x-app-layout>
     <div class="container pt-5">
+        <h1>Reservas</h1>
+        <hr>
+        <div class="text-end pb-2">
+            <a href="{{ route('orders.create')}}" class="btn btn-success" style="background-color: #11C163; border: none;">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                </svg>
+            </a>
+        </div>
         <table class="table text-center" id="orders_table" class="display">
             <thead>
                 <tr>
@@ -17,10 +27,10 @@
             <tbody >
                 @foreach ($orders as $order)
                 <tr>
-                    <th scope="row">1</th>
+                    <th scope="row">{{$order->id}}</th>
                     <td class="text-center">{{$order->order_date}}</td>
-                    <td class="text-center">{{$order->order_session}}</td>
-                    <td>{{$order->user->name}}</td>
+                    <td class="text-center">{{$order->order_hour}}</td>
+                    <td>{{$order->name}}</td>
                     <td>{{$order->service->type}}</td>
                     <td>
                         @if($order->is_online == 0) Barbería 

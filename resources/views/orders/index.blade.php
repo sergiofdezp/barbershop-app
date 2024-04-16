8fd8e4614d54d8ae05a07a3558928562be2b88fe
sergiof <sergiofdezp7@gmail.com>
2024-04-16 21:56:20 +0200

    consultar los detalles de cada reserva (stable)

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -3,8 +3,8 @@
         <h1>Reservas</h1>
         <hr>
         <div class="text-end pb-2">
-            <a href="{{ route('orders.create')}}" class="btn btn-success" style="background-color: #11C163; border: none;">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+            <a href="{{ route('orders.create')}}" class="btn btn-success" style="background-color: #13B807; border: none;">
+                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                     <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
                 </svg>
@@ -21,6 +21,7 @@
                     <th scope="col" class="text-center">Servicio</th>
                     <th scope="col" class="text-center">Precio</th>
                     <th scope="col" class="text-center">Estado del pago</th>
+                    <th scope="col" class="text-end">Acciones</th>
                 </tr>
             </thead>
             <tbody >
@@ -39,6 +40,16 @@
                         @elseif($order->pay_status == 1) Pagado
 
                         @endif
+                    </td>
+                    <td class="">
+                        <div class="flex justify-end">
+                            <a href="{{ route('orders.show', $order->id)}}" class="btn btn-dark btn-sm mx-1" style="background-color: #33BEFF; border: none;">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+                                    <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"/>
+                                    <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/>
+                                </svg>
+                            </a>
+                        </div>
                     </td>
                 </tr>
                 @endforeach
---
+++ resources/views/orders/show.blade.php
@@ -0,0 +1,83 @@
+<x-app-layout>
+    <div class="container pt-5">
+        <div class="row">
+            <div class="d-flex justify-content-between align-items-end">
+                <h1>Visualización de reserva</h1>
+                <p><span class="fw-bold">Referencia:</span> {{$order->order_ref}}</p>
+            </div>
+        </div>
+        <hr>
+        <div class="row pb-3">
+            <div class="text-end">
+                <a href="{{ route('orders.create')}}" class="btn btn-success" style="background-color: #2019FF; border: none;">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                    </svg>
+                </a>
+            </div>
+        </div>
+
+        <div class="row border p-5">
+            <div class="col-6">
+                <h5>Detalles de la reserva</h5>
+                <hr>
+                <p class="fw-bold">Servicio: <span class="fw-normal">{{$order->service->type}}.</span></p>
+                <p class="fw-bold">Día de la reserva: <span class="fw-normal">{{$order->order_date}}.</span></p>
+                <p class="fw-bold">Hora de la reserva: <span class="fw-normal">{{$order->order_hour}}h.</span></p>
+                <p class="fw-bold">Estado de la reserva: 
+                    <span class="fw-normal">
+                        @if($order->order_status == 0) Próxima.
+
+                        @elseif($order->order_status == 1) Terminada.
+
+                        @elseif($order->order_status == 2) Cancelada.
+
+                        @elseif($order->order_status == 3) No asistida.
+
+                        @endif
+                    </span>
+                </p>
+            </div>
+            <div class="col-6">
+                <h5>Detalles del cliente</h5>
+                <hr>
+                <p class="fw-bold">Nombre del cliente: <span class="fw-normal">{{$order->name}}.</span></p>
+                <p class="fw-bold">Teléfono del cliente: <span class="fw-normal">{{$order->phone}}.</span></p>
+            </div>
+        </div>
+
+        <div class="row border p-5 mt-3">
+            <div class="col-6">
+                <h5>Otros detalles</h5>
+                <hr>
+                <p class="fw-bold">Usuario que ha realizado la reserva: <span class="fw-normal">{{$order->user->name}}.</span></p>
+                <p class="fw-bold">Lugar de reserva:
+                    <span class="fw-normal">
+                        @if($order->is_online == 0) Barbería.
+    
+                        @elseif($order->pay_status == 1) Web.
+    
+                        @endif
+                    </span>
+                </p>
+                <p class="fw-bold">Fecha de creación de la reserva: <span class="fw-normal">{{$order->created_at}}.</span></p>
+                <p class="fw-bold">Última actualización: <span class="fw-normal">{{$order->updated_at}}.</span></p>
+            </div>
+            <div class="col-6">
+                <h5>Detalles del pago</h5>
+                <hr>
+                <p class="fw-bold">Precio total: <span class="fw-normal">{{$order->total_price}}€.</span></p>
+                <p class="fw-bold">Estado del pago:
+                    <span class="fw-normal">
+                        @if($order->pay_status == 0) Pendiente.
+    
+                        @elseif($order->pay_status == 1) Pagado.
+    
+                        @endif
+                    </span>
+                </p>
+            </div>
+        </div>
+    </div>
+</x-app-layout>

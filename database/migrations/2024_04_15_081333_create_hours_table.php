edd961ab801aadbf0e05eb0767c95b67a968a2c3
sergiof <sergiofdezp7@gmail.com>
2024-04-23 17:59:27 +0200

    Cambio en la migración de las horas

--- database/migrations/2024_04_15_081333_create_hours_table.php
+++ database/migrations/2024_04_15_081333_create_hours_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('hours', function (Blueprint $table) {
             $table->id();
             $table->string('order_hour');
-            $table->timestamps();
+            // $table->timestamps();
         });
     }
 

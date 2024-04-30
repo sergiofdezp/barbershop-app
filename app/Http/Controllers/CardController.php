3aae8ab8653b1c595b7c272865c2cc555279b601
sergiof <sergiofdezp7@gmail.com>
2024-04-30 12:38:43 +0200

    Sistema de generación de nuevas tarjetas y de rellenado de servicios en tarjetas en curso (stable)

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Card;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -27,9 +28,29 @@ class CardController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store($user_id)
     {
-        //
+        $card = array(
+            "user_id" => $user_id,
+            "num_services" => 0,
+            "available" => 0,
+            "used" => 0,
+        );
+
+        Card::create($card);
+    }
+
+    public function store_for_new_user()
+    {
+        $user_id = User::latest('id')->first()->id + 1;
+        $card = array(
+            "user_id" => $user_id,
+            "num_services" => 0,
+            "available" => 0,
+            "used" => 0,
+        );
+
+        Card::create($card);
     }
 
     /**
@@ -58,12 +79,23 @@ class CardController extends Controller
 
     public function update_num_services($user_id)
     {
-        $card = Card::where('user_id', '=', $user_id)->first();
+        $card = Card::where('user_id', '=', $user_id)->where('available', '=', 0)->first();
 
         return $card->update([
             'num_services' => $card->num_services + 1,
-            'available' => 0,
-            'used' => 0,
+            'available' => $card->available,
+            'used' => $card->used,
+        ]);
+    }
+
+    public function update_available_card($user_id)
+    {
+        $card = Card::where('user_id', '=', $user_id)->where('available', '=', 0)->first();
+
+        return $card->update([
+            'num_services' => $card->num_services,
+            'available' => 1,
+            'used' => $card->used,
         ]);
     }
 
--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -68,8 +68,22 @@ class OrderController extends Controller
         $order = $request->all();
         Order::create($order);
 
+        // Implementación de sistema de fidelización.
         $card_controller = new CardController;
-        $card_controller->update_num_services($request->user_id);
+
+        // Obtenemos la tarjeta en curso.
+        $num_services = Card::where('user_id', '=', $request->user_id)->where('available', '=', 0)->first();
+
+        // Update de tarjeta en curso o creación de nueva tarjeta.
+        // Si la tarjeta no ha completado los 8 servicios se seguirán incrementando hasta ser 8.
+        if($num_services->num_services < 8) {
+            $card_controller->update_num_services($request->user_id);
+        }
+        // En el caso de que la tarjeta tenga 8 servicios, se cambiará su estado 'available' a 1 y se generará una nueva.
+        else{
+            $card_controller->update_available_card($request->user_id);
+            $card_controller->store($request->user_id);
+        }
 
         if($user->id == 1){
             return redirect()->route('orders.index')->banner('Reserva añadida correctamente.');

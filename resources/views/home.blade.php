75adfbad9e661da0c99f608d0807d1a8d451f409
sergiof <sergiofdezp7@gmail.com>
2024-05-13 18:57:36 +0200

    cambios en el home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,6 +14,9 @@
         @livewireStyles
         <link rel="stylesheet" href="{{asset('/css/home.css')}}">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
         <div class="container-fluid d-flex justify-content-center align-items-center blur-background">
@@ -21,12 +24,25 @@
                 <div id="test">
                     @if (Route::has('login'))
                         @auth
-                            <a href="{{ url('/user_orders') }}" class="btn btn-dark" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Mis reservas">Mis reservas</a>
+                            <a href="{{ url('/user_orders') }}" class="inline-flex items-center px-4 py-2 bg-indigo-500 border border-transparent rounded-md font-semibold
+                                text-xs text-white uppercase tracking-widest hover:bg-indigo-700 focus:bg-indigo-700 active:bg-indigo-900
+                                focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150" 
+                                data-bs-toggle="tooltip" data-bs-placement="bottom" title="Mis reservas">
+                                Mis reservas
+                            </a>
                         @else
-                            <a href="{{ route('login') }}" class="btn btn-dark">Inicia sesión</a>
+                            <a href="{{ route('login') }}" class="inline-flex items-center px-4 py-2 bg-indigo-500 border border-transparent rounded-md font-semibold
+                                text-xs text-white uppercase tracking-widest hover:bg-indigo-700 focus:bg-indigo-700 active:bg-indigo-900
+                                focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                                Inicia sesión
+                            </a>
 
                             @if (Route::has('register'))
-                                <a href="{{ route('register') }}" class="btn btn-secondary">Crear una cuenta</a>
+                                <a href="{{ route('register') }}" class="inline-flex items-center px-4 py-2 bg-azure-500 border border-transparent rounded-md font-semibold
+                                    text-xs text-white uppercase tracking-widest hover:bg-azure-700 focus:bg-azure-700 active:bg-azure-900
+                                    focus:outline-none focus:ring-2 focus:ring-azure-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                                    Crear una cuenta
+                                </a>
                             @endif
                         @endauth
                     @endif
@@ -34,21 +50,6 @@
             </div>
         </div>
 
-        <!-- <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 selection:bg-red-500 selection:text-white">
-            @if (Route::has('login'))
-                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-                    @auth
-                        <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
-                    @else
-                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-        </div> -->
         <!-- Scripts -->
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
